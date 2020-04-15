f5c812f669599c7e66bfca0c119162617d26092a
Narayanan Rengaswamy <narayanan.rengaswamy@duke.edu>
2020-04-15 00:19:56 -0400

    Revert "Added more codes"
    This reverts commit a6a1af80fe0664c23ee6eae5253fcba486663271.

--- GF-FFT Code for GF(p^m)/gfpow.m
+++
@@ -1,8 +0,0 @@
-function [res] = gfpow(a,b,field)
-% Computes a^b in GF(p^m) represented as field
-res = 0;   % 0 is 1 in exponential format
-for times = 1:b
-    res = gfmul(res,a,field);
-end
-return
-end
